c766305957542a433a7d9f9c706d8842a246e5c2
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:28:42 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I ___ my hair, I ___ my teeth.",
+        options:["am not wash / am brushing", "am not washing / am brushing", "am not washing / am brush", "don't wash / brush"],
+        answer: 1
+    },
+    {
+     q:"I ___ at the moment.",
+        options:["am not working", "am not work", "don't work", "doesn't work"],
+        answer: 0   
+    },
+    {
+        q:"You ___ to me now.",
+        options:["doesn't listen", "don't listen", "aren't listening", "aren't listen"],
+        answer: 2
+    },
+    {
+        q:"They ___ to school. They are going to the theatre.",
+        options:["isn't going", "aren't go", "don't going", "aren't going"],
+        answer: 3
+    },
+    {
+        q:"We ___ breakfast today.",
+        options:["don't having", "are not have", "aren't having", "don't have"],
+        answer: 2
+    },
+    {
+        q:"He ___ at the moment.",
+        options:["don't sleep", "isn't sleeping", "isn't sleep", "aren't sleeping"],
+        answer: 1
+    },
+    {
+        q:"She ___  up early today.",
+        options:["isn't getting", "isn't get", "don't get", "aren't getting"],
+        answer: 0
+    },
+    {
+        q:"My mum ___ . She ___ TV.",
+        options:["aren't reading / are watching", "isn't read / watch", "doesn't reading / watches", "isn't reading / is watching"],
+        answer: 3
+    },
+    {
+        q:"My friend ___ the guitar right now.",
+        options:["don't play", "isn't playing", "aren't playing", "not play"],
+        answer: 1
+    },
+    {
+        q:"It ___ . It is snowing.",
+        options:["aren't raining", "doesn't rains", "is not raining", "isn't rain"],
+        answer: 2
+    }
+
+]
